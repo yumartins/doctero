43f78f5020279a325ca7d534e29271a3665ebcab
yumartins <develop@yumartins.com.br>
2021-02-08 06:53:45 -0300

    refactor(server): change stock migration

--- apps/server/src/database/migrations/02_stock.ts
+++ apps/server/src/database/migrations/02_stock.ts
@@ -2,15 +2,16 @@ import * as Knex from 'knex';
 
 import knexfile from '../../../knexfile';
 
-exports.up = async (knex: Knex): Promise<void> => knex.schema.createTable('products', (table) => {
+exports.up = async (knex: Knex): Promise<void> => knex.schema.createTable('stock', (table) => {
   table.increments('id').primary();
   table.text('name').notNullable();
   table.float('price').notNullable();
+  table.integer('amount').notNullable();
 
   table.integer('user_id').references('users.id').notNullable().onDelete('CASCADE');
 
   table.timestamps(true, true);
 })
-  .then(() => knex.raw(knexfile.updated('products')));
+  .then(() => knex.raw(knexfile.updated('stock')));
 
-exports.down = async (knex: Knex): Promise<void> => knex.schema.dropTable('products');
+exports.down = async (knex: Knex): Promise<void> => knex.schema.dropTable('stock');
