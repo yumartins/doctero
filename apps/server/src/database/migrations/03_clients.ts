76996f901544a7d9eda3bbaff5ab6c52a83a47e0
yumartins <develop@yumartins.com.br>
2021-02-11 19:53:40 -0300

    feat(server): clients migrations

--- apps/server/src/database/migrations/03_clients.ts
+++ apps/server/src/database/migrations/03_clients.ts
@@ -2,7 +2,7 @@ import * as Knex from 'knex';
 
 import knexfile from '../../../knexfile';
 
-exports.up = async (knex: Knex): Promise<void> => knex.schema.createTable('users', (table) => {
+exports.up = async (knex: Knex): Promise<void> => knex.schema.createTable('clients', (table) => {
   table.increments('id').primary();
 
   table.text('name').notNullable();
@@ -11,16 +11,15 @@ exports.up = async (knex: Knex): Promise<void> => knex.schema.createTable('users
   table.text('company');
   table.text('document').unique().notNullable();
 
+  table.text('note');
   table.text('phone');
   table.text('address');
   table.date('birthday');
-  table.text('password').notNullable();
 
-  table.integer('role_id').references('roles.id');
   table.integer('company_id').references('users.id');
 
   table.timestamps(true, true);
 })
-  .then(() => knex.raw(knexfile.updated('users')));
+  .then(() => knex.raw(knexfile.updated('clients')));
 
-exports.down = async (knex: Knex): Promise<void> => knex.schema.dropTable('users');
+exports.down = async (knex: Knex): Promise<void> => knex.schema.dropTable('clients');
