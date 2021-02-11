4cbb17c2a7cee6fe7e01639406b5418d9a332757
yumartins <develop@yumartins.com.br>
2021-02-11 20:02:26 -0300

    feat(server): services migrate

---
+++ apps/server/src/database/migrations/04_services.ts
@@ -0,0 +1,19 @@
+import * as Knex from 'knex';
+
+import knexfile from '../../../knexfile';
+
+exports.up = async (knex: Knex): Promise<void> => knex.schema.createTable('services', (table) => {
+  table.increments('id').primary();
+
+  table.text('sku');
+  table.text('name').notNullable();
+
+  table.integer('sale_price').notNullable();
+
+  table.integer('company_id').references('users.id');
+
+  table.timestamps(true, true);
+})
+  .then(() => knex.raw(knexfile.updated('services')));
+
+exports.down = async (knex: Knex): Promise<void> => knex.schema.dropTable('services');
