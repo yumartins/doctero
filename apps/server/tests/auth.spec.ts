92d8e55a5af5639f1c3e0fd31cd90630618e3e8e
Yuri Martins <develop@yumartins.com.br>
2021-03-21 07:29:39 -0300

    test(server): unregistered email

--- apps/server/tests/auth.spec.ts
+++ apps/server/tests/auth.spec.ts
@@ -9,7 +9,7 @@ import request from 'supertest';
 import app from '../src';
 
 describe('Testing authentication scenarios.', () => {
-  it('logged user data and status 200 must be returned', async () => {
+  it('logged user data and status 200 must be returned.', async () => {
     const res = await request(app)
       .post('/api/auth')
       .send({
@@ -19,6 +19,18 @@ describe('Testing authentication scenarios.', () => {
 
     expect(res.status).toEqual(200);
     expect(res.body).toHaveProperty('token');
+  });
+
+  it('log in with invalid user and return invalid email.', async () => {
+    const res = await request(app)
+      .post('/api/auth')
+      .send({
+        email: 'yuri@meuemail.com',
+        password: '123456',
+      });
+
+    expect(res.status).toEqual(401);
+    expect(res.body).toHaveProperty('message');
   });
 
   afterAll(async (done) => app.close(done));
